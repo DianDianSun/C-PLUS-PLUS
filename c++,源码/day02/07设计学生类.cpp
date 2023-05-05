9cf3ab0dfa6897533cb5f195331fdc89d7814f7d
Zhao Xuri <2589038857@qq.com>
2023-05-05 11:03:42 +0800

    new

--- c++,源码/day02/07设计学生类.cpp
+++ c++,源码/day02/07设计学生类.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class Student{
+	
+private://私有权限
+	string name;
+	string ID;
+public:
+	void Get_name_and_ID(string a,string b){
+		name = a;
+		ID = b;
+	}
+	void My_print(){
+		cout << name << endl;
+		cout << ID << endl;
+	}
+};
+
 int main() {
-	
-	
+	Student dian;
+	dian.Get_name_and_ID("diandian","040825");
+	dian.My_print();
 	return 0;
 }
