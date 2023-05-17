2ade566f3ffff5cf031b92fc747ab9a5ed152a0d
Zhao Xuri <2589038857@qq.com>
2023-05-17 17:19:21 +0800

    new

---
+++ c++,源码/day05/01数组类.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class MyArray{
+public:
+	MyArray(){
+		
+	}
+	MyArray(const MyArray &a){
+		
+	}
+	MyArray(int capacity,int val = 0){
+		
+	}
+	~MyArray(){
+		
+	}
+	//头插
+	void PushFront(int val){
+		
+	}
+	void PushBack(int val){
+		
+	}
+	void PopFront(){
+		
+	}void PopBack(){
+		
+	}
+	int Size(){
+		
+	}
+	void Insert(int pos,int val){
+		
+	}
+	int &Get(int pos){
+		
+	}
+	void Set
+private:
+	int *pArray;
+	int mSize;
+	int mCapacity;
+};
+
+int main() {
+	
+	
+	
+	return 0;
+}
